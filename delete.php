c35fc9eaaeeeea2a158842222aa8f749d5cf38d2
Dev Alexandre <131310553+Dev-AlexandreS@users.noreply.github.com>
2024-02-27 14:41:43 -0300

    Update delete.php

--- delete.php
+++ delete.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resultado da exclusão</title>
-    <link rel="stylesheet" href="..\desafios\d001\style.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
